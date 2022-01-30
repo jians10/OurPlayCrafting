c2256383c625be7884bd943e8862054733ad328d
AnastasiaYe <anastasia612@yahoo.com>
2022-01-29 19:33:15 -0500

    revert

--- GGCDemo/Assets/Script/AI/EnemyAIGround.cs
+++ GGCDemo/Assets/Script/AI/EnemyAIGround.cs
@@ -7,32 +7,56 @@ public class EnemyAIGround : MonoBehaviour
     // Start is called before the first frame update\
     public float speed;
     public float distance;
-    private bool movingRight = true;
+    public bool movingRight = true;
     public Transform groundDetection;
     private float damageAmount=11;
+    private Rigidbody2D rb;
     public LayerMask ground;
+    public Transform groundDetect;
+    public Transform posLeft, posRight;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
-        RaycastHit2D groundinfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance,ground);
-        if (groundinfo.collider == false) {
-            if (movingRight) {
-                transform.eulerAngles = new Vector3(0, 180, 0);
-                movingRight = false;
-            }
-            else
+        //if()
+        RaycastHit2D groundinfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, ground);
+        RaycastHit2D grounddetect = Physics2D.Raycast(groundDetect.position, Vector2.down, 2f, ground);
+        
+        if (Vector2.Distance(transform.position, posLeft.position)<1) {
+            movingRight=true;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        }
+        if (Vector2.Distance(transform.position,posRight.position)<1)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            movingRight =false;
+        }
+
+        if (grounddetect)
+        {
+            if (groundinfo.collider == false)
             {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = true;
+                if (movingRight)
+                {
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
             }
         }
     }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -45,6 +69,16 @@ public class EnemyAIGround : MonoBehaviour
             //player.Damage(damageAmount);
         }//
 
+    }
+    void Flip()
+    {
+        movingRight = !movingRight;
+        //transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+        //facingRight = !facingRight;
+        Vector3 Scalar = transform.localScale;
+        Scalar.x *= -1;
+        transform.localScale = Scalar;
+        //CreateDust();
     }
 
 
