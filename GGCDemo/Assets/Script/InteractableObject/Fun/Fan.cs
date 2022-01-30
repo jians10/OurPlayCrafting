24ab5293ef21bfe878c8400f3bba4b86397f15d4
Anastasia Ye <anastasia612@yahoo.com>
2022-01-30 14:28:06 -0500

    L3 Adjust

--- GGCDemo/Assets/Script/InteractableObject/Fun/Fan.cs
+++ GGCDemo/Assets/Script/InteractableObject/Fun/Fan.cs
@@ -6,14 +6,23 @@ public class Fan : MonoBehaviour
 {
     public LayerMask TargetLayer;
     public LayerMask BlockLayer;
-    public float suckforce;
+    [SerializeField] private float suckforceX;
+    [SerializeField] private float suckforceY;
     protected Vector2 AffectSize;
     protected Vector2 RelifSize;
+    protected float AffectDistance;
+    protected float RelifDistance;
+    public float VelocityReduceIndex;
+    public float xVelocityReduce = 1;
+
+
     protected float currdistance;
     public float affectprop;
     public float relifprop;
-    public float width;
-    public float AffectDistance=5;
+    public float affectwidth;
+    public float relifwidth;
+
+    public float FanDistance = 5;
     public Transform detectpos;
 
     public float relifOffset;
@@ -28,6 +37,9 @@ public class Fan : MonoBehaviour
     public bool showGizmo;
     //public bool suck;
     public bool active;
+    protected Vector2 direction;
+    protected Vector2 RelifCenterDraw;
+    protected Vector2 AffectCenterDraw;
 
     // Start is called before the first frame update
     void Start()
@@ -39,26 +51,40 @@ public class Fan : MonoBehaviour
     // Update is called once per frame
     virtual public void Update()
     {
-        RaycastHit2D lengthcheck = Physics2D.Raycast(detectpos.position, -detectpos.up, AffectDistance, BlockLayer);
+        direction = (-detectpos.up);
+        RaycastHit2D lengthcheck = Physics2D.Raycast(detectpos.position, direction, FanDistance, BlockLayer);
 
         if (lengthcheck)
         {
 
-            currdistance = Mathf.Abs(lengthcheck.transform.position.y - detectpos.position.y);
+            //currdistance = //Mathf.Abs((lengthcheck.transform.position - detectpos.position).magnitude)*0.8f;
+            currdistance = lengthcheck.distance;
         }
-        else {
+        else
+        {
 
-            currdistance = AffectDistance;
+            currdistance = FanDistance;
         }
-        float AffectSizey = (currdistance - affectOffset - relifOffset) / (affectprop + relifprop) * affectprop;
-        float RelifSizey = (currdistance - affectOffset - relifOffset) / (affectprop + relifprop) * relifprop;
 
-        AffectSize = new Vector2(width, AffectSizey);
-        RelifSize = new Vector2(width, RelifSizey);
+        AffectDistance = (currdistance - affectOffset - relifOffset) / (affectprop + relifprop) * affectprop;
+        RelifDistance = (currdistance - affectOffset - relifOffset) / (affectprop + relifprop) * relifprop;
+
+        AffectSize = new Vector2(affectwidth, AffectDistance);
+        RelifSize = new Vector2(relifwidth, RelifDistance);
+
+        RelifCenterDraw = (Vector2)(detectpos.transform.position) + direction * relifOffset + direction * RelifDistance / 2;
+        AffectCenterDraw = RelifCenterDraw + direction * AffectDistance / 2 + direction * affectOffset + direction * RelifDistance / 2; //+ direction * AffectDistance/2
+
+        RelifCenter = (Vector2)(detectpos.transform.position) + direction * relifOffset; //+ direction * RelifDistance / 2;
 
 
-        RelifCenter = new Vector2(transform.position.x, detectpos.position.y - relifOffset - RelifSize.y / 2);
-        AffectCenter = new Vector2(transform.position.x, RelifCenter.y - RelifSize.y / 2 - affectOffset - AffectSize.y / 2);
+
+        AffectCenter = RelifCenter + direction * RelifDistance + direction * affectOffset; //+ direction * AffectDistance/2;
+
+
+
+        //RelifCenter = new Vector2(transform.position.x, detectpos.position.y - relifOffset - RelifSize.y / 2);
+        //AffectCenter = new Vector2(transform.position.x, RelifCenter.y - RelifSize.y / 2 - affectOffset - AffectSize.y / 2);
 
         if (active)
         {
@@ -67,52 +93,62 @@ public class Fan : MonoBehaviour
         }
     }
 
-    public void activate() {
+    public void activate()
+    {
         active = true;
         Debug.Log("I am an activated fun");
         particleeffect.SetActive(true);
         lightindicator.SetActive(true);
-    
+
     }
 
-    public void deactivate() {
+    public void deactivate()
+    {
         active = false;
         particleeffect.SetActive(false);
         lightindicator.SetActive(false);
     }
 
-    
+
 
 
     virtual public void SuckDetect()
     {
-        Collider2D[] AttackTarget = Physics2D.OverlapBoxAll(AffectCenter,AffectSize, 0, TargetLayer);
+        //Collider2D[] AttackTarget = Physics2D.OverlapBoxAll(AffectCenter,AffectSize, 0, TargetLayer);
+
+        RaycastHit2D[] AttackTarget = Physics2D.BoxCastAll(AffectCenter, new Vector2(affectwidth, 0.2f), 0f, direction, AffectDistance, TargetLayer);
+
         if (AttackTarget.Length > 0)
         {
-            foreach (Collider2D target in AttackTarget)
+            foreach (RaycastHit2D target in AttackTarget)
             {
-                Vector2 direction = (transform.position - target.transform.position).normalized;
-                if (target.gameObject.GetComponent<Rigidbody2D>())
+                Vector2 forcedirection = (detectpos.position - target.transform.position).normalized;
+
+                //forcedirection = new Vector2(forcedirection.x, forcedirection.y);
+                if (target.transform.gameObject.GetComponent<Rigidbody2D>())
                 {
-                    target.gameObject.GetComponent<Rigidbody2D>().AddForce(direction * suckforce, ForceMode2D.Impulse);
+                    target.transform.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(forcedirection.x * suckforceX, forcedirection.y * suckforceY), ForceMode2D.Impulse);
 
                 }
             }
         }
     }
-    virtual public  void PushDetect() {
+    virtual public void PushDetect()
+    {
 
-        Collider2D[] AttackTarget = Physics2D.OverlapBoxAll(RelifCenter, RelifSize, 0, TargetLayer);
+        RaycastHit2D[] AttackTarget = Physics2D.BoxCastAll(RelifCenter, new Vector2(relifwidth, 0.2f), 0f, direction, RelifDistance, TargetLayer);
+
+        //Collider2D[] AttackTarget = Physics2D.OverlapBoxAll(RelifCenter, RelifSize, 0, TargetLayer);
         if (AttackTarget.Length > 0)
         {
-           // Debug.Log("something to rise");
-            foreach (Collider2D target in AttackTarget)
+            // Debug.Log("something to rise");
+            foreach (RaycastHit2D target in AttackTarget)
             {
                 Vector2 direction = (transform.position - target.transform.position).normalized;
-                Rigidbody2D rb = target.gameObject.GetComponent<Rigidbody2D>();
+                Rigidbody2D rb = target.transform.gameObject.GetComponent<Rigidbody2D>();
                 if (rb)
                 {
-                    rb.velocity =Vector2.Lerp(rb.velocity, new Vector2(rb.velocity.x,0), Time.deltaTime*5);
+                    rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(rb.velocity.x / xVelocityReduce, 0), Time.deltaTime * VelocityReduceIndex);
                 }
             }
         }
@@ -122,9 +158,19 @@ public class Fan : MonoBehaviour
     {
         if (showGizmo)
         {
-            //Gizmos.color = Color.blue;
-            Gizmos.DrawCube(AffectCenter, AffectSize);
-            Gizmos.DrawCube(RelifCenter, RelifSize);
+            if (Mathf.Abs(direction.y) > 0.2f)
+            {
+                //Gizmos.color = Color.blue;
+                Gizmos.DrawCube(AffectCenterDraw, AffectSize);
+                Gizmos.DrawCube(RelifCenterDraw, RelifSize);
+            }
+            else
+            {
+
+                Gizmos.DrawCube(AffectCenterDraw, new Vector2(AffectSize.y, AffectSize.x));
+                Gizmos.DrawCube(RelifCenterDraw, new Vector2(RelifSize.y, RelifSize.x));
+
+            }
         }
     }
 
--- GGCDemo/Assets/Script/InteractableObject/Fun/FunPush.cs
+++ GGCDemo/Assets/Script/InteractableObject/Fun/FunPush.cs
@@ -13,28 +13,35 @@ public class FunPush : Fan
     // Update is called once per frame
     override public void Update()
     {
-
-        RaycastHit2D lengthcheck = Physics2D.Raycast(detectpos.position, detectpos.up, AffectDistance, BlockLayer);
+        
+        direction = (detectpos.up);
+        RaycastHit2D lengthcheck = Physics2D.Raycast(detectpos.position, direction, FanDistance, BlockLayer);
 
         if (lengthcheck)
         {
 
-            currdistance = Mathf.Abs(lengthcheck.transform.position.y - detectpos.position.y);
+            //currdistance = //Mathf.Abs((lengthcheck.transform.position - detectpos.position).magnitude)*0.8f;
+            currdistance = lengthcheck.distance;
         }
         else
         {
 
-            currdistance = AffectDistance;
+            currdistance = FanDistance;
         }
-        float AffectSizey = (currdistance - affectOffset - relifOffset) / (affectprop + relifprop) * affectprop;
-        float RelifSizey = (currdistance - affectOffset - relifOffset) / (affectprop + relifprop) * relifprop;
 
-        AffectSize = new Vector2(width, AffectSizey);
-        RelifSize = new Vector2(width, RelifSizey);
+        AffectDistance = (currdistance - affectOffset - relifOffset) / (affectprop + relifprop) * affectprop;
+        RelifDistance = (currdistance - affectOffset - relifOffset) / (affectprop + relifprop) * relifprop;
+
+        AffectSize = new Vector2(affectwidth, AffectDistance);
+        RelifSize = new Vector2(relifwidth, RelifDistance);
+
+        AffectCenter = (Vector2)(detectpos.transform.position) + direction * affectOffset;// + direction * AffectDistance / 2;
+        RelifCenter = AffectCenter + direction *AffectDistance +  relifOffset*direction; //direction * RelifDistance / 2
 
 
-        AffectCenter = new Vector2(transform.position.x, detectpos.position.y + affectOffset + AffectSize.y / 2);
-        RelifCenter = new Vector2(transform.position.x, AffectCenter.y + RelifSize.y / 2 + relifOffset + AffectSize.y / 2);
+        AffectCenterDraw = (Vector2)(detectpos.transform.position) + direction * affectOffset + direction * AffectDistance / 2;
+        RelifCenterDraw = AffectCenterDraw + direction * relifOffset + direction * RelifDistance / 2+direction*AffectDistance/2;
+
 
         if (active)
         {
@@ -48,16 +55,16 @@ public class FunPush : Fan
     override public void SuckDetect()
     {
         //Debug.Log("Detected Enemey");
-        Collider2D[] AttackTarget = Physics2D.OverlapBoxAll(AffectCenter, AffectSize, 0, TargetLayer);
+        RaycastHit2D[] AttackTarget = Physics2D.BoxCastAll(AffectCenter, new Vector2(affectwidth, 0.2f), 0f, direction, AffectDistance, TargetLayer);
         if (AttackTarget.Length > 0)
         {
             // Debug.Log("something to rise");
-            foreach (Collider2D target in AttackTarget)
+            foreach (RaycastHit2D target in AttackTarget)
             {
                 //Vector2 direction = (transform.position - target.transform.position).normalized;
-                if (target.gameObject.GetComponent<Rigidbody2D>())
+                if (target.transform.gameObject.GetComponent<Rigidbody2D>())
                 {
-                    target.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * pushforce, ForceMode2D.Impulse);
+                    target.transform.gameObject.GetComponent<Rigidbody2D>().AddForce(direction * pushforce, ForceMode2D.Impulse);
 
                 }
             }
@@ -66,17 +73,17 @@ public class FunPush : Fan
     override public void PushDetect()
     {
 
-        Collider2D[] AttackTarget = Physics2D.OverlapBoxAll(RelifCenter, RelifSize, 0, TargetLayer);
+        RaycastHit2D[] AttackTarget = Physics2D.BoxCastAll(RelifCenter, new Vector2(relifwidth, 0.2f), 0f, direction, RelifDistance, TargetLayer);
         if (AttackTarget.Length > 0)
         {
             // Debug.Log("something to rise");
-            foreach (Collider2D target in AttackTarget)
+            foreach (RaycastHit2D target in AttackTarget)
             {
                 Vector2 direction = (transform.position - target.transform.position).normalized;
-                Rigidbody2D rb = target.gameObject.GetComponent<Rigidbody2D>();
+                Rigidbody2D rb = target.transform.gameObject.GetComponent<Rigidbody2D>();
                 if (rb)
                 {
-                    rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(rb.velocity.x, 0), Time.deltaTime * 5);
+                    rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(rb.velocity.x, 0), Time.deltaTime * VelocityReduceIndex);
                 }
             }
         }
@@ -86,12 +93,22 @@ public class FunPush : Fan
 
     private void OnDrawGizmos()
     {
-        if (showGizmo)
-        {
-            //Gizmos.color = Color.blue;
-            Gizmos.DrawCube(AffectCenter, AffectSize);
-            Gizmos.DrawCube(RelifCenter, RelifSize);
-        }
+       if (showGizmo)
+       {
+           if (Mathf.Abs(detectpos.up.y)>0.2f)
+            {
+                //Gizmos.color = Color.blue;
+                Gizmos.DrawCube(AffectCenterDraw, AffectSize);
+                Gizmos.DrawCube(RelifCenterDraw, RelifSize);
+            }
+            else
+            {
+
+                Gizmos.DrawCube(AffectCenterDraw, new Vector2(AffectSize.y, AffectSize.x));
+                Gizmos.DrawCube(RelifCenterDraw, new Vector2(RelifSize.y, RelifSize.x));
+
+            }
+       }
     }
 
 }
